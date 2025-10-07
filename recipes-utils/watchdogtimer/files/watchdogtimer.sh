461e706aed9ab1f52f1ff8da1c5fb4a3ab91f0f4
Luca Lanzillotta <luquilanzi@gmail.com>
2025-10-07 23:47:48 +0000

    got a few GPIOS to work, made a mess regardless

--- recipes-utils/watchdogtimer/files/watchdogtimer.sh
+++ recipes-utils/watchdogtimer/files/watchdogtimer.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-GPIO=30
-GPIO_CHIP=1
+#GPIO=30
+#GPIO_CHIP=1
 
 # Ping loop — toggle every second
-while true; do
-    gpioset --chip gpiochip$GPIO_CHIP $GPIO=1
+#while true; do
+ #   gpioset --chip gpiochip$GPIO_CHIP $GPIO=1
     echo "Pet High"
     sleep 0.5
-    gpioset --chip gpiochip$GPIO_CHIP $GPIO=0
+   # gpioset --chip gpiochip$GPIO_CHIP $GPIO=0
     sleep 0.5
     echo "Pet Low"
 done
